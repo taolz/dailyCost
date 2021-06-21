8c26d6b7827917f0443fe8e0c8dd925aef1506c8
taolz <taolz@outlook.com>
2021-06-21 23:58:07 +0800

    test

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    "postcss-pxtorem": {
-      rootValue: 37.5, // Vant 官方根字体大小是 37.5
-      propList: ["*"],
-      selectorBlackList: [".norem"], // 过滤掉.norem-开头的 class，不进行 rem 转换
-    },
-  },
-};
